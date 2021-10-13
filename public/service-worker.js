6b63ed13ca8f307088cc9eff3792cef5fb421781
janayAl <jsallen298705@gmail.com>
2021-10-13 10:13:53 -0500

    added files from public into the files to cache in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,13 @@
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/favorites.html',
+  '/index.js',
   '/topic.html',
-  '/assets/css/style.css',
-  '/dist/app.bundle.js',
-  '/dist/favorites.bundle.js',
-  '/dist/topic.bundle.js',
-  'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+  '/style.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon=512x512.png',
+  //   'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
+  //   'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
 ];
 
 const PRECACHE = 'precache-v1';
